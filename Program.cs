ab2aa15a26c77e92c14c9d5329e59993ab856061
unknown <truongvandinhhuy@gmail.com>
2021-12-25 13:19:17 +0700

    chuoi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace Lab8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Truong Van Dinh Huy");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Mời bạn nhập 1 chuỗi: ");
+            string input = Console.ReadLine();
+            System.Console.Write("Mời bạn nhập kí tự: ");
+            char kytu = char.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == kytu)
+                {
+                    count++;
+                }
+            }
+            System.Console.WriteLine("Số lần ký tự {0} xuất hiện trong chuổi là {1}", kytu, count);
         }
     }
 }
